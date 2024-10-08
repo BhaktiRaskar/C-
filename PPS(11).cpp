9f14a533633caa9d8fa517327d6c651027bbfbad
BhaktiRaskar <bhakti.raskar.7200@gmail.com>
2024-10-08 19:10:53 +0530

    Add files via upload
    It is the type of Buit in function or Library Function By this function we can easily know the power of any number . Here also cmath is important because all mathematical functions need cmath.

---
+++ PPS(11).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+  int num=2;
+  int exp=3;
+  cout<<"2 is raised to the power 3 is:"<<pow(2,3)<<endl;
+  return 0;
+  
+}
